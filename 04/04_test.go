8fe44d325075382e56087375764cc8e4c75ad3a7
rafaelgomez <rafaelgomez@facephi.com>
2023-04-19 16:34:18 +0200

    changed test 04

--- 04/04_test.go
+++ 04/04_test.go
@@ -9,17 +9,18 @@ func TestFindMajor(t *testing.T) {
 	var useCases = []struct {
 		name  string
 		input []int
+		major int
 	}{
-		{"One", []int{11, 6, 2, 10, 4}},
-		{"Two", []int{6, 5, 2, 1, 4}},
-		{"Three", []int{10, 6, 9, 8, 7}},
-		{"Four", []int{140, 101, 130, 120, 110}},
+		{"One", []int{2, 10, 11, 6, 4}, 2},
+		{"Two", []int{6, 5, 2, 1, 4}, 0},
+		{"Three", []int{9, 8, 7, 10, 6}, 3},
+		{"Four", []int{110, 101, 130, 120, 140}, 4},
 	}
 
 	for i, element := range useCases {
 		memory := findMajor(element.input)
 
-		if memory == &element.input[0] {
+		if memory == &element.input[element.major] {
 			t.Logf(`Test %s passed OK`, element.name)
 		} else {
 			t.Errorf("Expected: %v, got: %v", &element.input[i], memory)
